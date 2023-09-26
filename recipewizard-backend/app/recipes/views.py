68f509861f66c204bc7f313206c18cc73e0a0e92
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2023-09-26 09:11:08 +0100

    update serializers

--- recipewizard-backend/app/recipes/views.py
+++ recipewizard-backend/app/recipes/views.py
@@ -14,105 +14,109 @@ ingredient_schema = IngredientSchema()
 ingredients_schema = IngredientSchema(many=True)
 
 
-@bp.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "GET":
-        recipes = db.session.query(Recipe).all()
-        return recipes_schema.dump(recipes)
-    elif request.method == "POST":
-        try:
-            recipe_schema.load(request.json)
-        except ValidationError as err:
-            response = make_response(err.messages, 400)
-            return response
+@bp.route("/", methods=["GET"])
+def get_recipes():
+    recipes = Recipe.query.all()
+    return recipes_schema.dump(recipes)
 
-        name = request.json["name"]
-        prep_time = request.json["prep_time"]
-        servings = request.json["servings"]
-        calories = request.json["calories"]
-        steps = request.json["steps"]
-        new_ingredients = []
 
-        for ingredient in request.json["ingredients"]:
-            ingredient_name = ingredient["name"]
-            ingredient_quantity = ingredient["quantity"]
-            ingredient_unit = ingredient["unit"]
-            queried_ingredient = (
-                db.session.query(Ingredient)
-                .filter(
-                    Ingredient.name == ingredient_name,
-                    Ingredient.quantity == ingredient_quantity,
-                    Ingredient.unit == ingredient_unit,
-                )
-                .first()
+@bp.route("/", methods=["POST"])
+def create_recipe():
+    try:
+        recipe_schema.load(request.json)
+    except ValidationError as err:
+        response = make_response(err.messages, 400)
+        return response
+
+    name = request.json["name"]
+    prep_time = request.json["prep_time"]
+    servings = request.json["servings"]
+    calories = request.json["calories"]
+    steps = request.json["steps"]
+    new_ingredients = []
+
+    for ingredient in request.json["ingredients"]:
+        ingredient_name = ingredient["name"]
+        ingredient_quantity = ingredient["quantity"]
+        ingredient_unit = ingredient["unit"]
+        queried_ingredient = (
+            db.session.query(Ingredient)
+            .filter(
+                Ingredient.name == ingredient_name,
+                Ingredient.quantity == ingredient_quantity,
+                Ingredient.unit == ingredient_unit,
             )
-
-            if queried_ingredient:
-                new_ingredients.append(queried_ingredient)
-            else:
-                new_ingredient = Ingredient(
-                    name=ingredient_name,
-                    quantity=ingredient_quantity,
-                    unit=ingredient_unit,
-                )
-                new_ingredients.append(new_ingredient)
-
-        new_recipe = Recipe(
-            name=name, prep_time=prep_time, servings=servings, calories=calories, steps=steps
+            .first()
         )
-        new_recipe.ingredients.extend(new_ingredients)
-        db.session.add(new_recipe)
-        db.session.commit()
-        return recipe_schema.dump(new_recipe)
+
+        if queried_ingredient:
+            new_ingredients.append(queried_ingredient)
+        else:
+            new_ingredient = Ingredient(
+                name=ingredient_name,
+                quantity=ingredient_quantity,
+                unit=ingredient_unit,
+            )
+            new_ingredients.append(new_ingredient)
+
+    new_recipe = Recipe(
+        name=name,
+        prep_time=prep_time,
+        servings=servings,
+        calories=calories,
+        steps=steps,
+    )
+    new_recipe.ingredients.extend(new_ingredients)
+    db.session.add(new_recipe)
+    db.session.commit()
+    return recipe_schema.dump(new_recipe)
 
 
 @bp.route("/<int:recipe_id>/", methods=["GET"])
 def get_recipe(recipe_id):
     recipe = Recipe.query.get_or_404(recipe_id)
-    return recipe_schema.dump(recipe)
+    return jsonify(recipe_schema.dump(recipe))
 
 
 @bp.route("/<int:recipe_id>/", methods=["PUT"])
 def update_recipe(recipe_id):
     recipe = Recipe.query.get_or_404(recipe_id)
+    try:
+        recipe_schema.load(request.json)
+    except ValidationError as err:
+        response = make_response(err.messages, 400)
+        return response
 
-    if request.method == "PUT":
-        try:
-            recipe_schema.load(request.json)
-        except ValidationError as err:
-            response = make_response(err.messages, 400)
-            return response
+    recipe.name = request.json["name"]
+    recipe.prep_time = request.json["prep_time"]
+    recipe.servings = request.json["servings"]
+    recipe.calories = request.json["calories"]
+    new_ingredients = []
+    recipe.ingredients = []
 
-        recipe.name = request.json["name"]
-        recipe.prep_time = request.json["prep_time"]
-        recipe.servings = request.json["servings"]
-        recipe.calories = request.json["calories"]
-        new_ingredients = []
-        recipe.ingredients = []
-
-        for ingredient in request.json["ingredients"]:
-            ingredient_name = ingredient["name"]
-            ingredient_quantity = ingredient["quantity"]
-            ingredient_unit = ingredient["unit"]
-            queried_ingredient = (
-                db.session.query(Ingredient)
-                .filter(
-                    Ingredient.name == ingredient_name,
-                    Ingredient.quantity == ingredient_quantity,
-                    Ingredient.unit == ingredient_unit,
-                )
-                .first()
+    for ingredient in request.json["ingredients"]:
+        ingredient_name = ingredient["name"]
+        ingredient_quantity = ingredient["quantity"]
+        ingredient_unit = ingredient["unit"]
+        queried_ingredient = (
+            db.session.query(Ingredient)
+            .filter(
+                Ingredient.name == ingredient_name,
+                Ingredient.quantity == ingredient_quantity,
+                Ingredient.unit == ingredient_unit,
             )
+            .first()
+        )
 
-            if queried_ingredient:
-                new_ingredients.append(queried_ingredient)
-            else:
-                new_ingredient = Ingredient(
-                    name=ingredient_name,
-                    quantity=ingredient_quantity,
-                    unit=ingredient_unit,
-                )
-                new_ingredients.append(new_ingredient)
+        if queried_ingredient:
+            new_ingredients.append(queried_ingredient)
+        else:
+            new_ingredient = Ingredient(
+                name=ingredient_name,
+                quantity=ingredient_quantity,
+                unit=ingredient_unit,
+            )
+            new_ingredients.append(new_ingredient)
 
         recipe.ingredients.extend(new_ingredients)
         db.session.add(recipe)
@@ -132,6 +136,5 @@ def delete_recipe(recipe_id):
 @bp.route("/ingredients/", methods=["GET"])
 def get_ingredients():
     if request.method == "GET":
-        # recipes = Recipe.query.all()
         ingredients = db.session.query(Ingredient).all()
         return ingredients_schema.dump(ingredients)
--- recipewizard-backend/app/schemas/recipe.py
+++ recipewizard-backend/app/schemas/recipe.py
@@ -6,30 +6,18 @@ from marshmallow import fields, validate
 allowed_units = ["qty", "tsp", "tbsp", "ml", "l", "g", "kg", "cup", "oz", "lb"]
 
 
-class IngredientSchema(ma.Schema):
+class IngredientSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Ingredient
         ordered = True
 
-    name = fields.Str()
-    quantity = fields.Decimal()
     unit = fields.Str(validate=validate.OneOf(allowed_units))
 
 
-class RecipeSchema(ma.Schema):
+class RecipeSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Recipe
         ordered = True
 
-    id = fields.Int(dump_only=True)
-    name = fields.Str()
-    calories = fields.Int(
-        required=True,
-        error_messages={
-            "required": {"message": "Calories field is required", "code": 400}
-        },
-    )
-    prep_time = fields.Str()
-    servings = fields.Int()
-    ingredients = fields.List(fields.Nested(IngredientSchema))
+    ingredients = ma.Nested(IngredientSchema, many=True)
     steps = fields.Dict()
